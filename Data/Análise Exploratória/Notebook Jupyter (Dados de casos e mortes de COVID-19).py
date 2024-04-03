b3e4fed092a04f19c3484c8abd823c5998687dba
GrupoProjetoAplicado <162191804+GrupoProjetoAplicado@users.noreply.github.com>
2024-04-03 18:21:48 -0300

    Add files via upload

--- Data/Análise Exploratória/Notebook Jupyter (Dados de casos e mortes de COVID-19).py
+++ Data/Análise Exploratória/Notebook Jupyter (Dados de casos e mortes de COVID-19).py
@@ -8,8 +8,11 @@ import seaborn as sns
 dados = pd.read_csv('https://raw.githubusercontent.com/GrupoProjetoAplicado/Projeto/main/Data/WHO-COVID-19-global-data.csv')
 
 # Métodos para explorar os dados
+# Exibe as primeiras linhas do arquivo
 print(dados.head())
 
+# Exibe o número de linhas e colunas (e contagem de valores não nulos), o tipo de dado e a quantidade de memória usada
 print(dados.info())
 
+# Exibe contagem de valores não nulos, média, desvio padrão, valor mínimo, quartis e valor máximo
 print(dados.describe())
